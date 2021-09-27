f74b4bfad2de5e9bbeb438901f6c8b966b74ad0f
Timo Grün <Timo.Gruen@uni-duesseldorf.de>
2021-09-27 13:40:34 +0200

    fix(conventions): remove spring dependencies

--- gradle/convention/build.gradle.kts
+++ gradle/convention/build.gradle.kts
@@ -10,8 +10,6 @@ repositories {
 dependencies {
     implementation("com.gradle.publish:plugin-publish-plugin:0.15.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.3.5.RELEASE")
-    implementation("io.spring.gradle:dependency-management-plugin:1.0.9.RELEASE")
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.4.31")
     implementation("org.jetbrains.kotlin:kotlin-noarg:1.4.31")
